50ff40ac1b87b105bc8e672b24aeb63bf4faf2f0
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2021-11-13 21:32:02 +0600

    update

--- src/pages/Dashboard/Review.js
+++ src/pages/Dashboard/Review.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 import review from "../../images/review.png";
 import { FaStar } from "react-icons/fa";
 import useAuth from "../../hooks/useAuth";
